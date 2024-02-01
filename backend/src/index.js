e147c339078eb601ae11381eb7286e650fb37a4d
HyRaXxItY <yasirmohammed122002@gmail.com>
2024-02-01 17:14:08 +0530

    smol

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import app from './app.js'
+import app from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+    path: "./env",
+});
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        });
+        console.log(process.env.MONGODB_URI);
     })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    });
